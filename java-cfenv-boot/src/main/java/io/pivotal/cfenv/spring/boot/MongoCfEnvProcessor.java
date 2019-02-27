18b0e757b1b9726e082a7ded9e85ec6ad4ab2457
Mark Pollack <mpollack@pivotal.io>
2019-02-26 20:14:35 -0500

    Add support for mongodb
    Fixes #7

---
+++ java-cfenv-boot/src/main/java/io/pivotal/cfenv/spring/boot/MongoCfEnvProcessor.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.pivotal.cfenv.spring.boot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import io.pivotal.cfenv.core.CfCredentials;
+import io.pivotal.cfenv.core.CfEnv;
+import io.pivotal.cfenv.core.CfService;
+import io.pivotal.cfenv.core.UriInfo;
+
+
+/**
+ * Retrieve redis properties from {@see CfCredentials} and set {@literal spring.redis} Boot properties.
+ *
+ * @author Mark Pollack
+ */
+public class MongoCfEnvProcessor implements CfEnvProcessor {
+
+	private static String mongoScheme = "mongodb";;
+
+	@Override
+	public List<CfService> findServices(CfEnv cfEnv) {
+		List<CfService> cfServices = cfEnv.findAllServices();
+		List<CfService> cfRedisServices = new ArrayList<>();
+
+		for (CfService service : cfServices) {
+			if (service.existsByTagIgnoreCase("mongodb") ||
+					service.existsByLabelStartsWith("mongolab") ||
+					service.existsByUriSchemeStartsWith(mongoScheme) ||
+					service.existsByCredentialsContainsUriField(mongoScheme)) {
+				cfRedisServices.add(service);
+			}
+		}
+
+		return cfRedisServices;
+	}
+
+	@Override
+	public void process(CfCredentials cfCredentials, Map<String, Object> properties) {
+		String uri = cfCredentials.getUri(mongoScheme);
+		UriInfo uriInfo = new UriInfo(uri);
+		properties.put("spring.data.mongodb.uri", cfCredentials.getUri(mongoScheme));
+	}
+
+	@Override
+	public CfEnvProcessorProperties getProperties() {
+		return CfEnvProcessorProperties.builder().propertyPrefixes("spring.data.mongodb").serviceName("MongoDB").build();
+	}
+
+}
