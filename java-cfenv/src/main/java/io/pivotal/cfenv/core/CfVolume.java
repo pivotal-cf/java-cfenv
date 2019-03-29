c9e26f1b2fe5c650e5cd6b87a554809b1fc87732
Paul Warren <paul.warren@emc.com>
2019-03-28 18:29:05 -0700

    Add support for CF Volume Services
    [#164283399](https://www.pivotaltracker.com/story/show/164283399)
    
    Signed-off-by: Paul Warren <paul.warren@emc.com>

---
+++ java-cfenv/src/main/java/io/pivotal/cfenv/core/CfVolume.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.pivotal.cfenv.core;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Access individual volume.
+ *
+ * @author Paul Warren
+ */
+public class CfVolume {
+
+	private static final String MODE_READONLY = "r";
+
+	private final Map<String, String> volumeData;
+
+	public CfVolume(Map<String, String> volumeData) {
+		Objects.requireNonNull(volumeData);
+		this.volumeData = volumeData;
+	}
+
+	public Map<String, String> getMap() {
+		return volumeData;
+	}
+
+	public Path getPath() {
+		return Paths.get(volumeData.get("container_dir"));
+	}
+
+	public Mode getMode() {
+		if (MODE_READONLY.equals(volumeData.get("mode"))) {
+			return Mode.READ_ONLY;
+		}
+		return Mode.READ_WRITE;
+	}
+
+	public enum Mode {
+		READ_ONLY,
+		READ_WRITE,
+	}
+}
