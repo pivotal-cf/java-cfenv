50a63faab6ab1799a0ed04f03367a758c866197f
David Duarte <102606398+daviddmd@users.noreply.github.com>
2023-08-28 14:19:08 +0200

    Add MariaDB service detection and replace MySQL JDBC prefix Update MySqlJdbcUrlCreator to add MariaDB schemes, tags and labels Update MySqlJdbcUrlCreator to override createJdbcUrl from AbstractJdbcUrlCreator Update MySqlJdbcUrlCreator to add detection of MariaDB service based on CF environment tags, labels and MariaDB uri Conditionally update the JDBC URL that was either generated or obtained from the CF Service if the current driver class name matches the MariaDB driver and any of the specific MariaDB detection cases Overload existsByLabelStartsWith method in CfService to allow checking if one of many labels matches the CF service label

--- java-cfenv-jdbc/src/main/java/io/pivotal/cfenv/jdbc/MySqlJdbcUrlCreator.java
+++ java-cfenv-jdbc/src/main/java/io/pivotal/cfenv/jdbc/MySqlJdbcUrlCreator.java
@@ -23,48 +23,72 @@ import io.pivotal.cfenv.core.UriInfo;
  * @author Mark Pollack
  */
 public class MySqlJdbcUrlCreator extends AbstractJdbcUrlCreator {
+    public static final String MYSQL_SCHEME = "mysql";
+    public static final String MARIADB_SCHEME = "mariadb";
+    public static final String[] MYSQL_SCHEMES = new String[]{MYSQL_SCHEME, MARIADB_SCHEME};
+    public static final String MYSQL_TAG = "mysql";
+    public static final String MARIADB_TAG = "mariadb";
+    public static final String[] MYSQL_TAGS = new String[]{MYSQL_TAG, MARIADB_TAG};
+    public static final String MYSQL_LABEL = "mysql";
+    public static final String MARIADB_LABEL = "mariadb";
+    public static final String[] MYSQL_LABELS = new String[]{MYSQL_LABEL, MARIADB_LABEL};
+    public static final String MYSQL_DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+    public static final String MARIADB_DRIVER_CLASS_NAME = "org.mariadb.jdbc.Driver";
 
-	public static final String MYSQL_SCHEME = "mysql";
+    @Override
+    public boolean isDatabaseService(CfService cfService) {
+        return jdbcUrlMatchesScheme(cfService, MYSQL_SCHEMES)
+                || cfService.existsByTagIgnoreCase(MYSQL_TAGS)
+                || cfService.existsByLabelStartsWith(MYSQL_LABELS)
+                || cfService.existsByUriSchemeStartsWith(MYSQL_SCHEMES)
+                || cfService.existsByCredentialsContainsUriField(MYSQL_SCHEMES);
+    }
 
-	public static final String MYSQL_TAG = "mysql";
+    private boolean isMariaDbService(CfService cfService) {
+        return jdbcUrlMatchesScheme(cfService, MARIADB_SCHEME)
+                || cfService.existsByTagIgnoreCase(MARIADB_TAG)
+                || cfService.existsByLabelStartsWith(MARIADB_LABEL)
+                || cfService.existsByUriSchemeStartsWith(MARIADB_SCHEME)
+                || cfService.existsByCredentialsContainsUriField(MARIADB_SCHEME);
+    }
 
-	public static final String MYSQL_LABEL = "mysql";
+    @Override
+    public String getDriverClassName() {
+        String driverClassNameToUse;
+        try {
+            driverClassNameToUse = MARIADB_DRIVER_CLASS_NAME;
+            Class.forName(driverClassNameToUse, false, getClass().getClassLoader());
+        }
+        catch (ClassNotFoundException e) {
+            try {
+                driverClassNameToUse = MYSQL_DRIVER_CLASS_NAME;
+                Class.forName(driverClassNameToUse, false, getClass().getClassLoader());
+            }
+            catch (ClassNotFoundException e2) {
+                return null;
+            }
+        }
+        return driverClassNameToUse;
+    }
 
-	@Override
-	public boolean isDatabaseService(CfService cfService) {
-		if (jdbcUrlMatchesScheme(cfService, MYSQL_SCHEME)
-				|| cfService.existsByTagIgnoreCase(MYSQL_TAG)
-				|| cfService.existsByLabelStartsWith(MYSQL_LABEL)
-				|| cfService.existsByUriSchemeStartsWith(MYSQL_SCHEME)
-				|| cfService.existsByCredentialsContainsUriField(MYSQL_SCHEME)) {
-			return true;
-		}
-		return false;
-	}
+    @Override
+    public String createJdbcUrl(CfService cfService) {
+        CfCredentials cfCredentials = cfService.getCredentials();
+        String jdbcUrl = (String) cfCredentials.getMap().get("jdbcUrl");
+        if (jdbcUrl == null) {
+            jdbcUrl = buildJdbcUrlFromUriField(cfCredentials);
+        }
+        if (isMariaDbService(cfService) && getDriverClassName().equals(MARIADB_DRIVER_CLASS_NAME)) {
+            jdbcUrl = jdbcUrl.replaceFirst("^(jdbc:mysql)", "jdbc:mariadb");
+        }
+        return jdbcUrl;
+    }
 
-	@Override
-	public String getDriverClassName() {
-		String driverClassNameToUse = null;
-		try {
-			driverClassNameToUse = "org.mariadb.jdbc.Driver";
-			Class.forName(driverClassNameToUse, false, getClass().getClassLoader());
-		} catch (ClassNotFoundException e) {
-			try {
-				driverClassNameToUse = "com.mysql.cj.jdbc.Driver";
-				Class.forName(driverClassNameToUse, false, getClass().getClassLoader());
-			} catch (ClassNotFoundException e2) {
-				return null;
-			}
-		}
-		return driverClassNameToUse;
-	}
-
-	@Override
-	public String buildJdbcUrlFromUriField(CfCredentials cfCredentials) {
-		UriInfo uriInfo = cfCredentials.getUriInfo(MYSQL_SCHEME);
-		return String.format("%s%s://%s%s/%s%s%s", JDBC_PREFIX, MYSQL_SCHEME,
-				uriInfo.getHost(), uriInfo.formatPort(), uriInfo.getPath(),
-				uriInfo.formatUserNameAndPasswordQuery(), uriInfo.formatQuery());
-	}
-
+    @Override
+    public String buildJdbcUrlFromUriField(CfCredentials cfCredentials) {
+        UriInfo uriInfo = cfCredentials.getUriInfo(MYSQL_SCHEME);
+        return String.format("%s%s://%s%s/%s%s%s", JDBC_PREFIX, MYSQL_SCHEME,
+                uriInfo.getHost(), uriInfo.formatPort(), uriInfo.getPath(),
+                uriInfo.formatUserNameAndPasswordQuery(), uriInfo.formatQuery());
+    }
 }
