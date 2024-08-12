4fa77fd07f279205b82296a52c46a91922c68905
Ed King <ed.king@broadcom.com>
2024-08-12 11:15:20 +0100

    Incorporate model capabilities into GenAI CfEnvProcessor
    * Valid as of GenAI for Tanzu Platform v0.6.0
    * Split the GenAICfEnvProcessor into two - one specific to chat and
      the other specific to embedding
    * This allows a developer to either make one binding to a model that
      supports both chat and embeddings, or to make two distinct bindings,
      one to a chat model and the other to an embeddings model
    * When binding to two different models, each model will have a different
      api key, which will be set accordingly on the corresponding
      spring.ai.openai.{chat,embedding}.* properties
    * Both processors additionally set the top-level property
      spring.ai.openai.api-key="redundant" in order to circumvent an error
      that is raised when configuring more specific api-keys on the chat and
      embedding properties
    * If desired, additional GenAI CfEnvProcessors may be written to cater to
      image and audio model options at a later time
    
    Signed-off-by: Ed King <ed.king@broadcom.com>

---
+++ java-cfenv-boot/src/main/java/io/pivotal/cfenv/spring/boot/GenAIChatCfEnvProcessor.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.pivotal.cfenv.spring.boot;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import io.pivotal.cfenv.core.CfCredentials;
+import io.pivotal.cfenv.core.CfService;
+
+/**
+ * Retrieve GenAI on Tanzu Platform properties from {@link CfCredentials} and
+ * set {@literal spring.ai.openai.chat}
+ * Boot properties.
+ *
+ * @author Stuart Charlton
+ * @author Ed King
+ **/
+public class GenAIChatCfEnvProcessor implements CfEnvProcessor {
+
+	@Override
+	public boolean accept(CfService service) {
+		boolean isGenAIService = service.existsByTagIgnoreCase("genai") || service.existsByLabelStartsWith("genai");
+		if (isGenAIService) {
+			ArrayList<String> modelCapabilities = (ArrayList<String>) service.getCredentials().getMap().get("model_capabilities");
+			return modelCapabilities.contains("chat");
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public void process(CfCredentials cfCredentials, Map<String, Object> properties) {
+		properties.put("spring.ai.openai.api-key", "redundant");
+
+		properties.put("spring.ai.openai.chat.base-url", cfCredentials.getString("api_base"));
+		properties.put("spring.ai.openai.chat.api-key", cfCredentials.getString("api_key"));
+		properties.put("spring.ai.openai.chat.options.model", cfCredentials.getString("model_name"));
+	}
+
+	@Override
+	public CfEnvProcessorProperties getProperties() {
+		return CfEnvProcessorProperties.builder()
+				.propertyPrefixes("spring.ai.openai.chat")
+				.serviceName("GenAI on Tanzu Platform (chat)")
+				.build();
+	}
+}
---
+++ java-cfenv-boot/src/test/java/io/pivotal/cfenv/spring/boot/GenAIChatCfEnvProcessorTests.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.pivotal.cfenv.spring.boot;
+
+import org.junit.Test;
+
+import io.pivotal.cfenv.test.AbstractCfEnvTests;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Stuart Charlton
+ * @author Ed King
+ */
+public class GenAIChatCfEnvProcessorTests extends AbstractCfEnvTests {
+
+	@Test
+	public void testGenAIBootPropertiesWithChatModelCapability() {
+		String TEST_GENAI_JSON_FILE = "test-genai-chat-model.json";
+
+		String EXPECTED_URI_FROM_JSON_FILE = "https://genai-proxy.tpcf.io";
+		String EXPECTED_TOKEN_FROM_JSON_FILE = "sk-KW5kiNOKDd_1dFxsAjpVa";
+		String EXPECTED_MODEL_FROM_JSON_FILE = "meta-llama/Meta-Llama-3-8B";
+
+		mockVcapServices(getServicesPayload(readTestDataFile(TEST_GENAI_JSON_FILE)));
+
+		assertThat(getEnvironment().getProperty("spring.ai.openai.api-key")).isEqualTo("redundant");
+
+		assertThat(getEnvironment().getProperty("spring.ai.openai.chat.base-url")).isEqualTo(EXPECTED_URI_FROM_JSON_FILE);
+		assertThat(getEnvironment().getProperty("spring.ai.openai.chat.api-key")).isEqualTo(EXPECTED_TOKEN_FROM_JSON_FILE);
+		assertThat(getEnvironment().getProperty("spring.ai.openai.chat.options.model")).isEqualTo(EXPECTED_MODEL_FROM_JSON_FILE);
+
+		assertThat(getEnvironment().getProperty("spring.ai.openai.embedding.options.model")).isNull();
+		assertThat(getEnvironment().getProperty("spring.ai.openai.image.options.model")).isNull();
+		assertThat(getEnvironment().getProperty("spring.ai.openai.audio.transcription.options.model")).isNull();
+		assertThat(getEnvironment().getProperty("spring.ai.openai.audio.speech.options.model")).isNull();
+	}
+}
